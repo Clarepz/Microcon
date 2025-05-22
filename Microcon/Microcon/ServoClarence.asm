91c7ca05b035ec6159627ca954b26b4fb84ed2a2
Clarepz <clarence.provenaz@gmail.com>
2025-05-22 16:59:11 +0200

    correction

--- Microcon/Microcon/ServoClarence.asm
+++ Microcon/Microcon/ServoClarence.asm
@@ -27,13 +27,15 @@ output_compare0:
 	inc		servocounter
 
 	cpi 	servocounter, 10
-	brlo	PC+2
-	clr		servocounter  
+	brlo	PC+3
+	clr		servocounter,
+	ldi		servochanel	0b00000001
+
 
 	cpi 	servocounter, 8
-	brlo	PC+2
-	clr 	servochanel 	
-	
+	brlo	PC+3
+	clr 	servochanel 
+
 
 	out		SREG, _sreg
 	reti
@@ -48,8 +50,8 @@ reset:
 	LDSP	RAMEND				; load stack pointer (SP)
 	OUTI	DDRB,0xff			; portB = output
 		
-	OUTI	ASSR,  (1<<AS0)		; clock from TOSC1 (external)
-	OUTI	TCCR0, (0<<CTC0)+6	;  CS0=6 CK	
+	;OUTI	ASSR,  (1<<AS0)		; clock from TOSC1 (external)
+	OUTI	TCCR0, (0<<CTC0)+3	;  CS0=6 CK	
 	
 	OUTI	OCR0,timer0-1		; Output Compare reg 0
 	
