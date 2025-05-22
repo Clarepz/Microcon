4a9a5945917ea04423e6f99e2d50b49067e1e43c
Clarepz <clarence.provenaz@gmail.com>
2025-05-22 13:55:46 +0200

    treshold

--- Microcon/Microcon/main2.asm
+++ Microcon/Microcon/main2.asm
@@ -1,7 +1,9 @@
 ; file target ATmega128L-4MHz-STK300
 ; semaphores: r25
-
-.def DISTANCETRESH = 900
+;===Definition===
+.equ DISTANCETRESH = 900
+.equ ISPEED = 170
+.def speed = c0
 
 .include "macros.asm"		; include macro definitions
 .include "definitions.asm"	; include register/constant definitions
@@ -9,6 +11,8 @@
 ; === interrupt table ===
 .org	0
 	jmp	reset
+.org INT0addr
+    jmp ext_int0
 .org 	ADCCaddr
 	jmp	ADCCaddr_sra
 .org	0x30
@@ -16,30 +20,36 @@
 .include "irDistanceMacro.asm"
 .include "printf.asm"
 .include "uart.asm"	
+.include "lcd.asm"
 
 reset:
 	OUTI	DDRC,0xff		; configure portC to output
     LDSP	RAMEND			; set up stack pointer (SP)
-    sei
 	rcall	UART0_init
+    rcall   LCD_init    
+    rcall   SPEED_init      ;init speed control
     IRSET
+    sei
     rjmp main
 
 main:
 	
     DISTANCEREAD            ; read distance in b1:b0
     WAIT_MS 100
-    PRINTF	UART0_putc		; print formatted
+    PRINTF	UART0_putc		; print printDistance
 	.db	CR,CR,"CACA=",FHEX2,b,"=",FDEC2,b,"    ",0
     MOV2 a1,a0,b1,b0
     LSR2 a1,a0              ; print on leds
     LSR2 a1,a0
     out PORTC, a0
 
-    cpi a0, DISTANCETRESH   ; chesk distance
+    cpi a0, DISTANCETRESH   ; check distance
     brsh wall
-    rjmp main
 
+    PRINTF	UART0_putc		; print speed
+	.db	CR,CR,"Speed=",FDEC2,c,"    ",0
+
+    rjmp main
 wall:
     OUTI PORTC, 0xff
     WAIT_MS 200
---
+++ Microcon/Microcon/speedControl.asm
@@ -0,0 +1,22 @@
+; file	int0.asm   target ATmega128L-4MHz-STK300		
+; purpose using INT0..INT3
+
+.include "macros.asm"		; include macros definitions
+.include "definitions.asm"	; include register/constant definition
+
+; === interrupt table ===
+.org	0x02
+	jmp	ext_int0
+.org	0x04
+	jmp	ext_int1
+; === interrupt service routines	
+ext_int0:
+	inc speed
+    reti
+ext_int1:
+    dec speed
+	reti
+; === initialization (reset) ====
+SPEED_init:
+	OUTI	EIMSK,0b00000011 	; enable INT0/1
+    ret
