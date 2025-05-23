deb8394cc81cc3028e472e97d51f3ca5360bbb83
Clarepz <clarence.provenaz@gmail.com>
2025-05-23 16:36:36 +0200

    main3

--- Microcon/Microcon/main3.asm
+++ Microcon/Microcon/main3.asm
@@ -1,17 +1,23 @@
 ; file target ATmega128L-4MHz-STK300
-; semaphores: r25
+
+
+; button on PORTD
+; IR distance PORTF
+; servo PORTB PIN 0,1 
+; LED matrix PORTC PIN,0
+
 ;===Definition===
 .equ DISTANCETRESH = 225
 .equ ISPEED = 170
 
 .equ servoZero  = 190
 .equ turnSpeed  = 10
-.equ turnTime   = 2000    ;ms
-.equ offsetTime = 2000    ;ms
+.equ turnTime   = 2000      ;ms
+.equ offsetTime = 2000      ;ms
 
-.def servospeed1 = r24
-.def servospeed2 = r25
-.def globalspeed = r8
+.def semaphore  = r12       ;d0
+.def globalspeed = r8       ;c0
+
 ; === interrupt table ===
 .org	0
 	jmp	reset
@@ -19,6 +25,8 @@
     jmp ext_int0
 .org INT1addr
 	jmp	ext_int1
+.org INT2addr
+    jmp ext_int2
 .org 	ADCCaddr
 	jmp	ADCCaddr_sra
 .org	0x30
@@ -26,33 +34,46 @@
 .include "macros.asm"		; include macro definitions
 .include "definitions.asm"	; include register/constant definitions
 
-.include "irDistanceMacro.asm"  ; uses b0-b1
+.include "irDistanceMacro.asm"          ; uses b0-b1
 .include "printf.asm"
 .include "uart.asm"	
 .include "lcd.asm"
-.include "speedControl.asm"     ;uses c0
+.include "speedStanbyControl.asm"       ; uses c0
 .include "Yann23cm.asm"
 ;================
-
-
-
 
 reset:
     LDSP	RAMEND			; set up stack pointer (SP)
 	OUTI	DDRC,0xff		; leds
 	rcall	ws2812b4_init
-    rcall   printSHappy
 	rcall	UART0_init
     rcall   LCD_init    
     rcall   SPEED_init      ;init speed control
     IRSET                   ;init le capteur de distance
+
+
+    clr globalspeed
+    clr semaphore
+
     sei
-    rjmp main
+    rjmp standby
+
+standby:
+    rcall printSSleepy
+    SERVOWI 0, servoZero    ;speed = 0
+    SERVOWI 1, servoZero
+    sbloop:
+        sbrc semaphore
+        rjmp main
+        rjmp sbLoop
+
 
 main:
-	
+    cbr semaphore
+    rcall   printSHappy
+	mainloop:
     DISTANCEREAD            ; read distance in b1:b0
-    WAIT_MS 100
+    WAIT_MS 20
 
     PRINTF	UART0_putc		; print printDistance
 	.db	CR,CR,"Distance=",FDEC2,b,"    ",0
@@ -64,8 +85,11 @@ main:
     DISTANCECOMPARE
     brsh wall  
 
-    SERVOW 0 , servoZero + globalspeed     ;set speed
-    SERVOW 1 , servoZero + globalspeed
+    ;set speed:
+    ldi    a0, globalspeed
+    ADDI   a0, servoZero
+    SERVOW 0 , a0     
+    SERVOW 1 , a0
 
     PRINTF	UART0_putc		; print speed
 	.db	CR,CR,"Speed=",FDEC2,c,"    ",0
@@ -73,7 +97,12 @@ main:
 	PRINTF	LCD		; print speed
 	.db	CR,CR,"Speed=",FDEC2,c,"    ",0
 
-    rjmp main
+    ;check if start stop button pressed:
+
+    sbrc semaphore
+    rjmp standby
+
+    rjmp mainloop
 
 
 wall:
@@ -101,10 +130,9 @@ wall:
     SERVOWI 0, servoZero+turnSpeed
     SERVOWI 1, servoZero-turnSpeed
     WAIT_MS turnTime
-
-    rcall   printSHappy
     rjmp    main
 dead:
     rcall printSDead
     SERVOWI 0, servoZero
     SERVOWI 1, servoZero
+
--- Microcon/Microcon/speedStandbyControl.asm
+++ Microcon/Microcon/speedStandbyControl.asm
@@ -7,9 +7,13 @@ ext_int0:
 ext_int1:
     dec c0
 	reti
+ext_int2: ; pause button clicked
+    sbr semaphore, 1
+    reti
+
 ; === initialization (reset) ====
 SPEED_init:
-	OUTI   EIMSK,0b00000011 	; enable INT0/1
-    ldi    w, 0b00001111
+	OUTI   EIMSK,0b00000111 	; enable INT0/1/2
+    ldi    w, 0b00111111
     sts    EICRA,w    ; set to flanc montant
     ret
