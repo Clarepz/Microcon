7d293c1316ce4df60b38368f05d79206512b59e3
Clarepz <clarence.provenaz@gmail.com>
2025-05-24 11:41:35 +0200

    main45

--- Microcon/Microcon/main4.asm
+++ Microcon/Microcon/main4.asm
@@ -1,11 +1,28 @@
 .include "macros.asm"
 .include "definitions.asm"
+; button on PORTD
+; IR distance PORTF
+; servo PORTB PIN 0,1 
+; LED matrix PORTE PIN,1
+
+;===Definition===
+.equ DISTANCETRESH = 225
+.equ ISPEED = 0
 
 
+.equ turnSpeed  = 80
+.equ turnTime   = 2000      ;ms
+.equ offsetTime = 2000      ;ms
 
-;======== table interupt =======
+.def semaphore  = r22       ;d0
+.def globalspeed = r23      ;c0
+
+; === interrupt table ===
 .org	0
 	jmp	reset
+
+
+.org	30
 
 .org OVF1addr
 	rjmp overflow1
@@ -16,15 +33,19 @@
 .org OC1Baddr
 	rjmp output_compare1b
 
-.org	30
+;======== bibliothï¿½ques =======
 
 
-;======== bibliothèques =======
 
-.include "lcd.asm"
-.include "Yann23cm.asm"
-.include "printf.asm"
+
 .include "servoDeYannLeBoss.asm"
+.include "irDistanceMacro.asm"          ; uses b0-b1
+.include "printf.asm"
+.include "uart.asm"	
+.include "lcd.asm"
+;.include "speedStandbyControl.asm"       ; uses c0
+.include "Yann23cm.asm"
+
 
 
 
@@ -36,19 +57,32 @@ reset:
 	rcall   LCD_init
 	SERVOSETUP
 
+	D_LED_INIT
+  
+    ;rcall   SPEED_init      ;init speed/standby control
+    IRSET                   ;init le capteur de distance
+
+    ldi globalspeed, ISPEED
+    clr semaphore
+
+    ;sei
+    rjmp main
+    ;rjmp standby
+
 
 ;======== main ========
 main:
+	
 	PRINTF LCD
 	.db	CR,CR,"Content",FDEC2,b,"    ",0
 	rcall printSHappy
-	SERVO1WI 100    ;speed = 0
-    SERVO2WI 100
+	SERVO1WI 2    ;speed = 0
+    SERVO2WI 2
 	WAIT_MS 2000
 	PRINTF LCD
 	.db	CR,CR,"Duper",FDEC2,b,"    ",0
 	rcall printSConcerned
-	SERVO1WI -50    ;speed = 0
-    SERVO2WI -50
+	SERVO1WI 4    ;speed = 0
+    SERVO2WI 4
 	WAIT_MS 2000
 	rjmp main
